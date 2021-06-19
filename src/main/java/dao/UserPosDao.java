e6ad527d389224f6dafdaf805fa37ba7b8246039
erick Monteiro <monteiro.ck@hotmail.com>
2021-06-19 12:30:50 -0300

    insert - JDBC

--- src/main/java/dao/UserPosDao.java
+++ src/main/java/dao/UserPosDao.java
@@ -1,8 +1,11 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import conexaojdbc.SingleConnection;
+import model.UserPosJava;
 
 public class UserPosDao {
 	
@@ -10,5 +13,32 @@ public class UserPosDao {
 	
 	public UserPosDao() {
 		connection = SingleConnection.getConnection();
+	}
+	
+	public void salvar (UserPosJava userposJava) {
+		
+		try {
+			String sql = "insert into userposjava(id,nome,email)values(?,?,?)";
+			// Prepara o Insert
+			PreparedStatement insert = connection.prepareStatement(sql);
+			//Estaticos os dados
+//			insert.setLong(1, 2);
+//			insert.setString(2, "Erick - Salvado Dao");
+//			insert.setString(3, "erick@teste.com");
+			
+			//Dinamicos os dados
+			insert.setLong(1,userposJava.getId());
+			insert.setString(2, userposJava.getNome());
+			insert.setString(3,userposJava.getEmail() );
+			insert.execute();
+			connection.commit(); //salva no bd
+		} catch (Exception e) {
+			try {
+				connection.rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+			e.printStackTrace();
+		}
 	}
 }
--- src/test/java/pos_java_jdbc/pos_java_jdbc/TesteBancoJdbc.java
+++ src/test/java/pos_java_jdbc/pos_java_jdbc/TesteBancoJdbc.java
@@ -3,11 +3,21 @@ package pos_java_jdbc.pos_java_jdbc;
 import org.junit.Test;
 
 import conexaojdbc.SingleConnection;
+import dao.UserPosDao;
+import model.UserPosJava;
 
 public class TesteBancoJdbc {
 	@Test
 	public void initBanco() {
-		SingleConnection.getConnection();
+		//SingleConnection.getConnection();
+		UserPosDao userPosDAO = new UserPosDao(); //obj dao
+		UserPosJava userposJava = new UserPosJava();
+		userposJava.setId(4L);
+		userposJava.setNome("Java");
+		userposJava.setEmail("java@mail.com");
+		userPosDAO.salvar(userposJava);
+		
+		
 	}
 
 }
